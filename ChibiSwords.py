96e29a062c832410563a96aa2f4c1cc358118167
jinxIsOp <154748750+jinxIsOp@users.noreply.github.com>
2024-08-09 23:21:10 +0100

    torch goblin l3ziz

--- ChibiSwords.py
+++ ChibiSwords.py
@@ -12,6 +12,7 @@ def load_and_scale_image(image_path, new_width, new_height):
     scaled_image = pygame.transform.scale(image, (new_width, new_height))
     return scaled_image
 
+# Load images
 base_img = load_and_scale_image("terrain/base.png", 120, 150)
 base1_img = load_and_scale_image("terrain/base1.png", 120, 150)
 base2_img = load_and_scale_image("terrain/base2.png", 120, 150)
@@ -26,19 +27,13 @@ flat2_img = load_and_scale_image("terrain/flat2.png", 120, 110)
 flat3_img = load_and_scale_image("terrain/flat2.png", 120, 110)
 tower1_img = load_and_scale_image("terrain/Tower_Blue.png", 100, 155)
 tower2_img = load_and_scale_image("terrain/Tower_purple.png", 100, 155)
-sand_img = load_and_scale_image("terrain/sand.png", 100, 100)
-sand1_img = load_and_scale_image("terrain/sand1.png", 100, 100)
-borders_img = pygame.image.load("terrain/borders.png")
-borders1_img = pygame.image.load("terrain/borders1.png")
 water = load_and_scale_image("terrain/Water.png", SCREEN_WIDTH, SCREEN_HEIGHT)
-sandBig = load_and_scale_image("terrain/sandBig.png", 600 , 300)
-bridge = load_and_scale_image("terrain/bridge.png",180,80)
-bridge1 = load_and_scale_image("terrain/bridge1.png",180,80)
-bridge2 = load_and_scale_image("terrain/bridge2.png",180,80)
-bridgeShadow = load_and_scale_image("terrain/BridgeShadow.png",130,80)
+bridge = load_and_scale_image("terrain/bridge.png", 180, 80)
+bridge1 = load_and_scale_image("terrain/bridge1.png", 180, 80)
 foam_images = [load_and_scale_image(f"terrain/Foam{i}.png", 140, 150) for i in range(1, 9)]
 archer_images = [load_and_scale_image(f"idleArcher/idle{i}.png", 50, 60) for i in range(1, 7)]
 knight_images = [load_and_scale_image(f"knightBlueRun/Warrior_Blue{i}.png", 50, 60) for i in range(1, 7)]
+torch_images = [load_and_scale_image(f"TorchRun/torch{i}.png", 60, 60) for i in range(1, 7)]
 
 foam_animation_index = 0
 foam_animation_speed = 0.1
@@ -48,11 +43,22 @@ archer_animation_index = 0
 archer_animation_speed = 0.1
 archer_animation_time = 0
 
+knight_animation_speed = 0.15
+knight_animation_time = 0
+
+torch_animation_speed = 0.15
+torch_animation_time = 0
+
+knights = [{"image_index": 0, "x": 180, "y": 265, "speed": 1}]
+torchs = [{"image_index": 0, "x": 865, "y": 265, "speed": -1}]
+
+def kill(obj, obj_list):
+    obj_list.remove(obj)
+
 def draw_scene():
-    global foam_animation_index, foam_animation_time, archer_animation_index, archer_animation_time
+    global foam_animation_index, foam_animation_time, archer_animation_index, archer_animation_time, knight_animation_time, torch_animation_time
 
     screen.blit(water, (0, 0))
-
 
     if pygame.time.get_ticks() > foam_animation_time:
         foam_animation_index = (foam_animation_index + 1) % len(foam_images)
@@ -62,8 +68,6 @@ def draw_scene():
     screen.blit(foam_images[foam_animation_index], (325, 320))
     screen.blit(foam_images[foam_animation_index], (580, 320))
     screen.blit(foam_images[foam_animation_index], (835, 320))
-
-
 
     screen.blit(base_img, (80, 300))
     screen.blit(base1_img, (80, 270))
@@ -79,11 +83,9 @@ def draw_scene():
     screen.blit(flat3_img, (845, 265))
     screen.blit(tower1_img, (85, 190))
     screen.blit(tower2_img, (865, 190))
-    screen.blit(bridge, (180,275))
-    screen.blit(bridge1, (435,275))
-    screen.blit(bridge1, (690,275))
-
-
+    screen.blit(bridge, (180, 275))
+    screen.blit(bridge1, (435, 275))
+    screen.blit(bridge1, (690, 275))
 
     if pygame.time.get_ticks() > archer_animation_time:
         archer_animation_index = (archer_animation_index + 1) % len(archer_images)
@@ -91,13 +93,30 @@ def draw_scene():
 
     screen.blit(archer_images[archer_animation_index], (110, 180))
 
+    for knight in knights[:]:
+        knight["x"] += knight["speed"]
 
-    if pygame.time.get_ticks() > archer_animation_time:
-        archer_animation_index = (archer_animation_index + 1) % len(archer_images)
-        archer_animation_time = pygame.time.get_ticks() + int(1000 * archer_animation_speed)
+        if pygame.time.get_ticks() > knight_animation_time:
+            knight["image_index"] = (knight["image_index"] + 1) % len(knight_images)
+            knight_animation_time = pygame.time.get_ticks() + int(1000 * knight_animation_speed)
 
-    screen.blit(knight_images[archer_animation_index], (180, 265))
-    
+        screen.blit(knight_images[knight["image_index"]], (knight["x"], knight["y"]))
+
+        if knight["x"] >= 865:
+            kill(knight, knights)
+
+    for torch in torchs[:]:
+        torch["x"] += torch["speed"]
+
+        if pygame.time.get_ticks() > torch_animation_time:
+            torch["image_index"] = (torch["image_index"] + 1) % len(torch_images)
+            torch_animation_time = pygame.time.get_ticks() + int(1000 * torch_animation_speed)
+
+        screen.blit(torch_images[torch["image_index"]], (torch["x"], torch["y"]))
+
+        if torch["x"] <= 85:
+            kill(torch, torchs)
+
 def main():
     clock = pygame.time.Clock()
     running = True
